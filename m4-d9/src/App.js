117b22b033eb165af12d4b6e4e764e9684bdb4ec
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-09-06 07:09:59 +0200

    updated

--- m4-d9/src/App.js
+++ m4-d9/src/App.js
@@ -4,42 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 // import MyBadge from './components/MyBadge'
 // import SingleBook from './components/SingleBook'
 import BookList from './components/BookList'
+import Registration from './components/Registration'
+import RegistrationAlt from './components/Registration_alt'
 import fantasyBooks from './fantasyBooks.json'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import NavBar from './components/NavBar'
-import Register from './components/Register'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar />
-
         <header className="App-header">
-
-          <Switch>
-            <Route path="/register" exact component={Register} />
-            <Route
-              path="/"
-              exact
-              render={() => (
-
-
-                <BookList books={fantasyBooks} />
-
-              )}
-            />
-
-          </Switch>
           {/* <WarningSign text="Watch out again!" /> */}
           {/* <MyBadge text="NEW!!" color="info" /> */}
           {/* <SingleBook book={fantasyBooks[0]} /> */}
+          <Route path="/" exact render={() => <BookList books={fantasyBooks} />} />
+          <Route path="/register" exact component={Registration} />
+          <Route path="/register-alt" exact component={RegistrationAlt} />
         </header>
-
       </div>
     </Router>
-
   )
 }
 
